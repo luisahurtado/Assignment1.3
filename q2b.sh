cb6d9c004bc56b889b2625a709d1f29a1b148ba9
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-04-15 22:18:10 +1000

    Update q2b.sh

--- q2b.sh
+++ q2b.sh
@@ -2,6 +2,6 @@
 # Assignment 1 task 3  question 2.b.
 # Execute the SQL query using Member table
 # First, you should have the Member table in the compbiol database 
-# The query looks for the members in which join year equals to 2010
-mysql -u awkologist -p compboil -e '\
+# The query looks for the members in which the joining year quals to 2010
+mysql -u awkologist -p compbiol -e '\
 select LastName, FirstName from Member where Year(JoinDate) = 2010; \'
