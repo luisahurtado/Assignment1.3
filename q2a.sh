5b1d36ef7d4a786fe3140cb5bfc4a51489e3ddd8
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-04-15 22:16:39 +1000

    Update q2a.sh

--- q2a.sh
+++ q2a.sh
@@ -2,6 +2,6 @@
 # Assignment 1 task 3  question 2.a.
 # Execute the SQL query using the Member table
 # First, you should have the Member table in the compbiol database 
-# The quey is looking for the members that have the Coach attribute empty.
-mysql -u awkologist -p compboil -e '\
+# The query is looking for the members that have empty the Coach attribute.
+mysql -u awkologist -p compbiol -e '\
 select m.LastName, m.FirstName from Member m where m.Coach IS NULL; \'
