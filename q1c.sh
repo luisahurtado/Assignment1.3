208d9f636e4393f2ae609a6dc2ffb0cfd464153d
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-04-15 22:14:38 +1000

    Update q1c.sh

--- q1c.sh
+++ q1c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Assignment 1 task 3  question 1.c.
 # Execute the Linux commands using the annotation and expression tables
-# The files does not have header. The commands make a join between annotation and 
+# The files must not have header. The commands make a join between annotation and 
 # expression tables showing the columns gene, function and exp_value
 sort -t$'\t' -k 1 annotation.tab > annotationSort.tab
 sort -t$'\t' -k 1 expression.tab > expressionSort.tab
