4a2604bb6147adf3adbb0df02bd17a7b8c331d73
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-04-15 22:20:06 +1000

    Update q2d.sh

--- q2d.sh
+++ q2d.sh
@@ -2,6 +2,6 @@
 # Assignment 1 task 3  question 2.d.
 # Execute the SQL query using Member and Entry tables
 # First, you should have the Member and Entry tables in the compbiol database
-# The query uses the Division operator to obtain the members who enter every year that the members competed
-mysql -u awkologist -p compboil -e '\
+# The query uses the division operator to obtain the members who enter every year that the members competed
+mysql -u awkologist -p compbiol -e '\
 select m.MemberId, m.LastName, m.FirstName from Member m where not exists (select distinct e.Year from Entry e where not exists (select * from Entry e2 where e2.MemberID=m.MemberId and e.Year=e2.Year)); \'
